ce6e3522c5f3464ecd7accd434ec76fd11a4127d
choppin6723 <nhhughes@wpi.edu>
2014-11-08 18:22:14 -0500

    added model and entity manager

--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/model/Task.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/model/Task.java
@@ -7,8 +7,11 @@ import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
+import com.google.gson.Gson;
+
 import edu.wpi.cs.wpisuitetng.modules.AbstractModel;
 //import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement; // ???? is happening here
+
 
 /**
  * @author nathan
@@ -16,18 +19,38 @@ import edu.wpi.cs.wpisuitetng.modules.AbstractModel;
  * @author jill
  */
 public class Task extends AbstractModel {
-	
+
 	String title;
 	String description;
 	TaskStatus status;
 	List<Member> assignedTo;
-	
+	int id;
 	Integer estimatedEffort; 
 	Integer actualEffort;
 	Date dueDate;
 	List<Activity> activities;
 	//List<Requirement> associatedRequirement;
-	
+
+
+	/**
+	 * Empty constructor for the Task class
+	 * 
+	 *
+	 */
+
+	Task() {
+		super();
+		this.title = "";
+		this.description = "";
+		this.status = null;
+		this.assignedTo = new LinkedList<Member>();
+		this.estimatedEffort = -1;
+		this.actualEffort = -1;
+		this.dueDate = null;
+		this.activities = new LinkedList<Activity>();
+	}
+
+
 	/**
 	 * Constructor for a task that has all of the fields
 	 * 
@@ -53,11 +76,11 @@ public class Task extends AbstractModel {
 		this.dueDate = dueDate;
 		this.activities = activities;
 	}
-	
+
 	/**
-		 * Constructor for a task that does not have list of members assigned to it or a list of activities
-		 * 
-		 * 
+	 * Constructor for a task that does not have list of members assigned to it or a list of activities
+	 * 
+	 * 
 	 * @param title name for the task
 	 * @param description explanation of the task
 	 * @param status what point the task is at (in progress, not started, etc.)
@@ -65,7 +88,7 @@ public class Task extends AbstractModel {
 	 * @param actualEffort number that represents the actual effort
 	 * @param dueDate when the task is due
 	 */
-	
+
 	Task(String title, String description, TaskStatus status, Integer estimatedEffort,
 			Integer actualEffort, Date dueDate) {
 		super();
@@ -78,7 +101,7 @@ public class Task extends AbstractModel {
 		this.dueDate = dueDate;
 		this.activities = new LinkedList<Activity>();
 	}
-	
+
 	/**
 	 * Constructor for a task with only a title
 	 * @param title name for the task
@@ -95,30 +118,173 @@ public class Task extends AbstractModel {
 		this.activities = new LinkedList<Activity>();
 	}
 
+
+
+	//database methods
+	/**
+	 * Returns an instance of Requirement constructed using the given
+	 * Requirement encoded as a JSON string.
+	 * 
+	 * @param json
+	 *            JSON-encoded Requirement to deserialize @return the
+	 *            Requirement contained in the given JSON
+	 */
+	public static Task fromJson(String json) {
+		return new Gson().fromJson(json, Task.class);
+
+	}
+
+	/**
+	 * Method toJSON. @return String * @see
+	 * edu.wpi.cs.wpisuitetng.modules.Model#toJSON() * @see
+	 * edu.wpi.cs.wpisuitetng.modules.Model#toJSON()
+	 */
+	@Override
+	/**This returns a Json encoded String representation of this requirement object.
+	 * 
+	 * @return a Json encoded String representation of this requirement
+	 * 
+	 */
+	public String toJson() {
+		return new Gson().toJson(this, Task.class);
+	}
+
+
 	
+
+	 /**
+     * Copies all of the values from the given requirement to this requirement.
+     * 
+     * @param toCopyFrom
+     *            the requirement to copy from.
+     */
+    public void copyFrom(Task toCopyFrom) {
+        //TODO Can't this be replaced by a default Java method?
+        //If not it can be with Requirement.fromJson(requirement.toJson)
+        this.description = toCopyFrom.description;
+        this.title = toCopyFrom.title;
+        this.status = toCopyFrom.status;
+        this.assignedTo = toCopyFrom.assignedTo;
+        this.id = toCopyFrom.id;
+        this.estimatedEffort = toCopyFrom.estimatedEffort;
+        this.actualEffort = toCopyFrom.actualEffort;
+        this.status = toCopyFrom.status;
+        this.dueDate = toCopyFrom.dueDate;
+        this.activities = toCopyFrom.activities;
+       
+    }
+	
+
 	//ignore this, don't delete 
 	@Override
 	public void save() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void delete() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	@Override
-	public String toJson() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	@Override
 	public Boolean identify(Object o) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+
+	
+	//GETTERS AND SETTERS 
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public TaskStatus getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(TaskStatus status) {
+		this.status = status;
+	}
+
+
+	public List<Member> getAssignedTo() {
+		return assignedTo;
+	}
+
+
+	public void setAssignedTo(List<Member> assignedTo) {
+		this.assignedTo = assignedTo;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public Integer getEstimatedEffort() {
+		return estimatedEffort;
+	}
+
+
+	public void setEstimatedEffort(Integer estimatedEffort) {
+		this.estimatedEffort = estimatedEffort;
+	}
+
+
+	public Integer getActualEffort() {
+		return actualEffort;
+	}
+
+
+	public void setActualEffort(Integer actualEffort) {
+		this.actualEffort = actualEffort;
+	}
+
+
+	public Date getDueDate() {
+		return dueDate;
+	}
+
+
+	public void setDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+	}
+
+
+	public List<Activity> getActivities() {
+		return activities;
+	}
+
+
+	public void setActivities(List<Activity> activities) {
+		this.activities = activities;
 	}
 
 }
