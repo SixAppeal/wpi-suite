f8b7921059fba721707ff1f50cc163006f50b8e7
danseaman6 <dpseaman@wpi.edu>
2014-11-09 14:46:31 -0500

    Initial Task Presenter

---
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/presenter/TaskPresenter.java
@@ -0,0 +1,37 @@
+package edu.wpi.cs.wpisuitetng.modules.taskmanager.presenter;
+
+import java.util.HashMap;
+
+
+/**
+ * 
+ * @author Will Van Rensselaer, Dan Seaman
+ */
+public class TaskPresenter implements IPresenter{
+
+	Gateway gateway;
+	
+	@Override
+	public void setGateway(Gateway gateway) {
+		this.gateway = gateway;
+	}
+
+	public void createTask(){
+		
+	}
+	
+	public void updateTask(){
+		
+	}
+	
+	public void getAllTasks(){
+		
+	}
+	
+	public void getTask(){
+		
+	}
+	
+	
+}
+
