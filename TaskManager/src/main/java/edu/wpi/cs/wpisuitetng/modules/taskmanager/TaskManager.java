af68563cad5f7d8c00ba77c9f0d0a2a918542696
William Van Rensselaer <wavanrensselaer@wpi.edu>
2014-11-08 22:45:28 -0500

    Initialize a gateway in TaskManager

--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/TaskManager.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/TaskManager.java
@@ -8,6 +8,7 @@ import javax.swing.JPanel;
 
 import edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule;
 import edu.wpi.cs.wpisuitetng.janeway.modules.JanewayTabModel;
+import edu.wpi.cs.wpisuitetng.modules.taskmanager.presenter.Gateway;
 
 /**
  * 
@@ -21,6 +22,7 @@ public class TaskManager implements IJanewayModule {
 	//Access Level: Package
 	String name;
 	List<JanewayTabModel> tabs;
+	Gateway gateway;
 	
 	/**
 	 * Constructs a TaskManager module and its tabs for the Janeway client.
@@ -29,6 +31,8 @@ public class TaskManager implements IJanewayModule {
 		
 		name = "Task Manager";
 		tabs = new ArrayList<JanewayTabModel>();
+		gateway = new Gateway();
+		
 		tabs.add(new JanewayTabModel(
 				name,
 				new ImageIcon(),
