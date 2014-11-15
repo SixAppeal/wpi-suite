a084229ba897775d44f64bb55cd233cce3f0c32d
Alexander Shoop <akshoop@wpi.edu>
2014-11-15 13:42:37 -0500

    Remove import of SimpleDateFormat

--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/sidebar/TaskEditView.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/sidebar/TaskEditView.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.swing.JButton;
