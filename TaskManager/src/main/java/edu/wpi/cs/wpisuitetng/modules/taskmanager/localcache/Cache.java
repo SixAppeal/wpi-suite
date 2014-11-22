e8d91308d0fa9afdcdc3de8be267e0e137c4da3c
Nathan Hughes <nhhughes@wpi.edu>
2014-11-21 22:50:23 -0500

    created cache interface and implementation

---
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/localcache/Cache.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package edu.wpi.cs.wpisuitetng.modules.taskmanager.localcache;
+
+import edu.wpi.cs.wpisuitetng.exceptions.NotImplementedException;
+
+/**
+ * Interface to describe cache functionality 
+ * 
+ * @author nhhughes
+ *
+ */
+public interface Cache {
+
+	/**
+	 * Takes a request that specifies where the object should be cached
+	 * @param request Task, Member or anything else that needs to be cached
+	 * @param toStore Object to be added to a type specific cache
+	 * @return whether the request was successful
+	 */
+	public boolean store(String request, Object toStore);
+	
+	/**
+	 * Takes a request that specifies where in the cache the object should be updated
+	 * @param request Task, Member, or anything else that needs to be cached
+	 * @param toUpdate Object to be updates to a type specific cache
+	 * @return whether the request was successful
+	 */
+	public boolean update(String request, Object toUpdate);
+	
+	/**
+	 * Takes a request that specifies an object to delete in the cache and deletes it.
+	 * @param request Task, Member, or anything else that needs to be cached
+	 * @param toDelete Object to be delete
+	 * @return whether the request was successful
+	 */
+	public boolean delete(String request, Object toDelete);
+	
+	/**
+	 * Takes a request for a certain cache and returns the cache
+	 * @param request the cache type wanted
+	 * @return a copy of the cache
+	 */
+	public Object[] retreive(String request);
+	
+	/**
+	 * Takes a request for a certain cache and a criteria to filter the cache by
+	 * and returns the results of the cache
+	 * @param request cache type
+	 * @param filter string, format to be determined later
+	 * @return array of objects satisfying cache
+	 * @throws NotImplementedException 
+	 */
+	public Object[] retreive(String request, String filter) throws NotImplementedException;
+	
+	/**
+	 * Deletes all entries from one of the caches.  
+	 * @param request Cache to delete from or 'all'
+	 * @return whether a cache was successfully selected or not
+	 */
+	public boolean clearCache(String request);
+}
---
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/localcache/LocalCache.java
@@ -0,0 +1,169 @@
+/**
+ * 
+ */
+package edu.wpi.cs.wpisuitetng.modules.taskmanager.localcache;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.wpi.cs.wpisuitetng.exceptions.NotImplementedException;
+import edu.wpi.cs.wpisuitetng.modules.core.models.User;
+import edu.wpi.cs.wpisuitetng.modules.taskmanager.model.Task;
+import edu.wpi.cs.wpisuitetng.modules.taskmanager.model.TaskStatus;
+
+/**
+ * This class is responsible for managing local copies of the database information
+ * @author nhhughes
+ *
+ */
+public class LocalCache implements Cache {
+	
+	private List<Task> tasks;
+	private List<Task> archive;
+	private List<User> members;
+	private List<TaskStatus> statuses;
+	
+	
+	/**
+	 * Initializes the local cache with a lookup table and some cache data structures
+	 * @author nhhughes
+	 */
+	public LocalCache() {
+		tasks = new ArrayList<Task>();
+		archive = new ArrayList<Task>();
+		members = new ArrayList<User>();
+		statuses = new ArrayList<TaskStatus>();
+	}
+	
+
+	/**
+	 * @see edu.wpi.cs.wpisuitetng.modules.taskmanager.localcache.ICache#store(java.lang.String, java.lang.Object)
+	 */
+	@Override
+	public boolean store(String request, Object toStore) {
+		if (request.compareTo("task") == 0) {
+			tasks.add((Task) toStore);
+			return true;
+		}
+		if (request.compareTo("archive") == 0) {
+			archive.add((Task) toStore);
+			return true;
+		}
+		if (request.compareTo("member") == 0) {
+			members.add((User) toStore);
+			return true;
+		}
+		if (request.compareTo("stage") == 0) {
+			statuses.add((TaskStatus) toStore);
+			return true;
+		}
+		return false;
+	}
+
+
+	/**
+	 * @see edu.wpi.cs.wpisuitetng.modules.taskmanager.localcache.ICache#update(java.lang.String, java.lang.Object)
+	 */
+	@Override
+	public boolean update(String request, Object toUpdate) {
+		if (request.compareTo("task") == 0) {
+			tasks.remove((Task) toUpdate);
+			tasks.add((Task) toUpdate);
+			return true;
+		}
+		if (request.compareTo("archive") == 0) {
+			archive.remove((Task) toUpdate);
+			archive.add((Task) toUpdate);
+			return true;
+		}
+		if (request.compareTo("member") == 0) {
+			members.remove((User) toUpdate);
+			members.add((User) toUpdate);
+			return true;
+		}
+		if (request.compareTo("stage") == 0) {
+			statuses.remove((TaskStatus) toUpdate);
+			statuses.add((TaskStatus) toUpdate);
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * @see edu.wpi.cs.wpisuitetng.modules.taskmanager.localcache.ICache#delete(java.lang.String, java.lang.Object)
+	 */
+	@Override
+	public boolean delete(String request, Object toDelete) {
+		if (request.compareTo("task") == 0) {
+			tasks.remove((Task) toDelete);
+			return true;
+		}
+		if (request.compareTo("archive") == 0) {
+			archive.remove((Task) toDelete);
+			return true;
+		}
+		if (request.compareTo("member") == 0) {
+			members.remove((User) toDelete);
+			return true;
+		}
+		if (request.compareTo("stage") == 0) {
+			statuses.remove((TaskStatus) toDelete);
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * @see edu.wpi.cs.wpisuitetng.modules.taskmanager.localcache.ICache#retreive(java.lang.String)
+	 */
+	@Override
+	public Object[] retreive(String request) {
+		if (request.compareTo("task") == 0) {
+			return tasks.toArray(new Task[0]);
+		}
+		if (request.compareTo("archive") == 0) {
+			return archive.toArray(new Task[0]);
+		}
+		if (request.compareTo("member") == 0) {
+			return members.toArray(new User[0]);
+		}
+		if (request.compareTo("stage") == 0) {
+			return members.toArray(new TaskStatus[0]);
+		}
+		return null;
+	}
+
+	/**
+	 * @throws NotImplementedException 
+	 * @see edu.wpi.cs.wpisuitetng.modules.taskmanager.localcache.ICache#retreive(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public Object[] retreive(String request, String filter) throws NotImplementedException {
+		throw new NotImplementedException();
+	}
+
+	/**
+	 * @see edu.wpi.cs.wpisuitetng.modules.taskmanager.localcache.ICache#clearCache(java.lang.String)
+	 */
+	@Override
+	public boolean clearCache(String request) {
+		if (request.compareTo("task") == 0) {
+			tasks = new ArrayList<Task>();
+			return true;
+		}
+		if (request.compareTo("archive") == 0) {
+			archive = new ArrayList<Task>();
+			return true;
+		}
+		if (request.compareTo("member") == 0) {
+			members = new ArrayList<User>();
+			return true;
+		}
+		if (request.compareTo("stage") == 0) {
+			statuses = new ArrayList<TaskStatus>();
+			return true;
+		}
+		return false;
+	}
+
+}
