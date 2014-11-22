4cd13a9fb5356b8efe0aacabbdc7ed2132a4b935
Santiago-Rojas <srojas@wpi.edu>
2014-11-22 17:06:45 -0500

    Changed the sidebarView to incorporate a list of views. Implemented the tabPane for the tabs. Also incorporated the tabs to the methods for adding a create and edit panel. @wavanrensselaer @srojas

--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/sidebar/SidebarView.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/sidebar/SidebarView.java
@@ -2,9 +2,12 @@ package edu.wpi.cs.wpisuitetng.modules.taskmanager.view.sidebar;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 import edu.wpi.cs.wpisuitetng.modules.taskmanager.model.Task;
 import edu.wpi.cs.wpisuitetng.modules.taskmanager.presenter.Gateway;
@@ -15,114 +18,79 @@ import edu.wpi.cs.wpisuitetng.modules.taskmanager.view.IView;
  * @author wavanrensselaer
  * @author akshoop
  * @author rnorlando
+ * @author srojas
+ * 
  */
 public class SidebarView extends JPanel implements IView {
 	private static final long serialVersionUID = -9157611802121055998L;
 
 	Gateway gateway;
-	
-	private JPanel container;
-	private JPanel curView;
-	private TaskDetailView detailView;
-	private TaskEditView editView;
-	private TaskCreateView createView;
+
+
 	private TaskDefaultView defaultView;
-	
+	private JTabbedPane tabPane;
+	private List<IView>  viewList; // list for views
+
 	/**
 	 * Constructs a sidebar view
 	 */
 	public SidebarView() {
-		this.container = new JPanel();
-		this.detailView = new TaskDetailView();
-		this.createView = new TaskCreateView();
-		this.editView = new TaskEditView();
+
+
 		this.defaultView = new TaskDefaultView();
-		
-		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		
-		this.container.setLayout(new BoxLayout(this.container, BoxLayout.X_AXIS));
-		this.container.setBackground(new Color(250, 250, 250));
-		this.container.setMinimumSize(new Dimension(300, 0));
-		this.container.setPreferredSize(new Dimension(300, 500));
-		this.container.setMaximumSize(new Dimension(300, Integer.MAX_VALUE));
-		
-		this.curView = this.defaultView;
-		this.createView.setVisible(false);
-		this.editView.setVisible(false);
-		this.detailView.setVisible(false);
-		
-		this.container.add(this.defaultView);
-		this.container.add(this.createView);
-		this.container.add(this.editView);
-		this.container.add(this.detailView);
-		this.add(container);
+
+		this.tabPane = new JTabbedPane(JTabbedPane.LEFT);
+		this.tabPane.addTab("Default View",this.defaultView);
+		this.viewList = new ArrayList<IView>();
+
+		this.setBackground(new Color(250, 250, 250));
+		this.setMinimumSize(new Dimension(300, 0));
+		this.setPreferredSize(new Dimension(300, 500));
+		this.setMaximumSize(new Dimension(300, Integer.MAX_VALUE));
+		this.add(tabPane);
 	}
-	
-	/**
-	 * Updates the sidebar with all members from the server
-	 * @param members All members available to assign to a task
-	 */
-	public void updateMembers(String member) {
-			this.editView.addMember(member);
-			this.createView.addMember(member);
-	}
-	
-	public void clearMembers() {
-		this.editView.setAllMembers(new String[0]);
-		this.createView.setAllMembers(new String[0]);
-	}
-	
-	/**
-	 * Shows the default panel
-	 */
-	public void showDefaultPanel() {
-		this.curView.setVisible(false);
-		this.curView = this.defaultView;
-		this.curView.setVisible(true);
-	}
+
+
 
 	/**
 	 * Shows the creation panel 
 	 */
-	public void showCreatePanel() {
-		this.curView.setVisible(false);
-		this.curView = this.createView;
-		this.curView.setVisible(true);
-		this.createView.updateView(new Task());
+	public void addCreatePanel() {
+		TaskCreateView createView = new TaskCreateView();
+		tabPane.addTab("Create Task", createView);
+		this.tabPane.setSelectedIndex(this.tabPane.indexOfComponent(createView));
+		this.viewList.add(createView);
 	}
-	
+
 	/**
 	 * Shows the edit panel
 	 * @param task The task to edit
 	 */
-	public void showEditPanel(Task task) {
-		this.editView.updateView(task);
-		this.curView.setVisible(false);
-		this.curView = this.editView;
-		this.curView.setVisible(true);
+	public void addEditPanel(Task task) {
+		TaskEditView editView = new TaskEditView();
+		tabPane.addTab("Create Task", editView);
+		this.tabPane.setSelectedIndex(this.tabPane.indexOfComponent(editView));
+
 	}
-	
+
 	/**
 	 * Shows the detail panel
 	 * @param task The task to display
 	 */
-	public void showDetailPanel(Task task) {
-		this.curView.setVisible(false);
-		this.curView = this.detailView;
-		this.detailView.updateView(task);
-		this.curView.setVisible(true);
-		this.detailView.revalidate();
+	public void addDetailPanel(Task task) {
+		TaskDetailView detailView = new TaskDetailView();
+		tabPane.addTab("Create Task", detailView);
+		this.tabPane.setSelectedIndex(this.tabPane.indexOfComponent(detailView));
+
 	}
-	
+
 	/**
 	 * @see IView.setGateway
 	 */
 	@Override
 	public void setGateway(Gateway gateway) {
 		this.gateway = gateway;
-		this.detailView.setGateway(this.gateway);
-		this.editView.setGateway(this.gateway);
-		this.createView.setGateway(this.gateway);
+
 	}
 
 }
