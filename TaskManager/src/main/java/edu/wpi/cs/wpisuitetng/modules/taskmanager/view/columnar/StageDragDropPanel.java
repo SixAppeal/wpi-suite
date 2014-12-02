036621f4fdb63a0e1654e4988c800480ef025e22
TJ Meehan <tmeehan@wpi.edu>
2014-12-01 22:35:02 -0500

    Documentation for files in the view folder
    authors: tmeehan dpseamen nhhughes

--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/columnar/StageDragDropPanel.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/columnar/StageDragDropPanel.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2014 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: Nathan Hughes, Thomas Meehan, Dan Seaman
+ ******************************************************************************/
+
 package edu.wpi.cs.wpisuitetng.modules.taskmanager.view.columnar;
 
 import edu.wpi.cs.wpisuitetng.modules.taskmanager.draganddrop.CustomDropTargetListener;
@@ -7,11 +18,24 @@ import java.awt.dnd.DropTarget;
 
 import javax.swing.JPanel;
 
+/**
+ * Container for the task views
+ * 
+ * @author nhhughes
+ * @author dpseaman
+ * @author tmeehan
+ *
+ */
+
 public class StageDragDropPanel extends JPanel {
 	
 	private static final long serialVersionUID = 6924509690789899864L;
 	private StageView view;
-
+	
+/**
+ * Constructor
+ * @param view parent container
+ */
 	public StageDragDropPanel(StageView view) {
 		super();
 		this.view = view;
@@ -20,7 +44,11 @@ public class StageDragDropPanel extends JPanel {
 		setDropTarget(new DropTarget(StageDragDropPanel.this,
 				new CustomDropTargetListener(StageDragDropPanel.this)));
 	}
-
+	
+	/**
+	 * getter
+	 * @return return the stage view
+	 */
 	public StageView getStageView() {
 		return view;
 	}
