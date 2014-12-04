f08db1b47a945d3e175b0f9827ea3477e7bf170b
thhughes <troy.h.hughes@gmail.com>
2014-12-03 20:18:37 -0500

    Fixed MemberListHandler tests; known bug in file.

--- TaskManager/src/test/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/components/TestMemberListHandler.java
+++ TaskManager/src/test/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/components/TestMemberListHandler.java
@@ -13,11 +13,8 @@ import org.junit.Test;
 import edu.wpi.cs.wpisuitetng.modules.core.models.User;
 import edu.wpi.cs.wpisuitetng.modules.taskmanager.view.sidebar.MemberListHandler;
 
-/*
+
 public class TestMemberListHandler {
-	MemberListHandler testHandler = new MemberListHandler();
-	
-	
 	
 	// Populate testUsernames in setup()
 	List<String> testUsernames = new ArrayList<String>();
@@ -42,96 +39,88 @@ public class TestMemberListHandler {
 		testUsernamesUnassigned.removeAll(testUsernamesAssigned);
 		
 		
-		
-
-		// Debug Print's
-//		System.out.println("Setup Unassigned: " +  this.testUsernamesUnassigned);
-//		System.out.println("Setup Assigned:   " +  this.testUsernamesAssigned);
-//		System.out.println("Setup Global:     " +  this.testUsernames);
+		//MemberListHandler.getInstance().
 	}
 	
 	@Test
 	public void testConstructor(){
-		assertEquals(this.testHandler.getAssigned(), new ArrayList<String>());
-		assertEquals(this.testHandler.getUnassigned(), new ArrayList<String>());
-		assertEquals(this.testHandler.getGlobal(), new ArrayList<String>());
+		assertEquals(MemberListHandler.getInstance().getAssigned(), new ArrayList<String>());
+		assertEquals(MemberListHandler.getInstance().getUnassigned(), new ArrayList<String>());
+		assertEquals(MemberListHandler.getInstance().getGlobal(), new ArrayList<String>());
 		
 	}
 	
 	@Test
 	public void testGetNumAssigned(){
-		assertEquals(this.testHandler.getNumAssigned(), new Integer(0));
-		assertEquals(this.testHandler.getNumUnAssigned(), new Integer(0));
-		assertEquals(this.testHandler.getNumMembers(), new Integer(0));
+		assertEquals(MemberListHandler.getInstance().getNumAssigned(), new Integer(0));
+		assertEquals(MemberListHandler.getInstance().getNumUnAssigned(), new Integer(0));
+		assertEquals(MemberListHandler.getInstance().getNumMembers(), new Integer(0));
 	}
 	
-	@Test
-	public void testGlobalAdder(){
-		this.testHandler.addGlobal("Username1");
-		assertEquals(this.testHandler.getNumMembers(), new Integer(1));
-	}
-	
-	@Test
-	public void testGlobalAdder_2(){
-		this.testHandler.addGlobal(testUsernames);
-		// System should not allow douplicates, therefore should be 4 members
-		assertEquals(this.testHandler.getNumMembers(), new Integer(4));
-		assertEquals(this.testHandler.getGlobal(), testUsernames);
-		
-	}
+//	@Test
+//	public void testGlobalAdder(){
+//		MemberListHandler.getInstance().addGlobal("Username1");
+//		assertEquals(MemberListHandler.getInstance().getNumMembers(), new Integer(1));
+//	}
+//	
+//	@Test
+//	public void testGlobalAdder_2(){
+//		MemberListHandler.getInstance().addGlobal(testUsernames);
+//		// System should not allow douplicates, therefore should be 4 members
+//		assertEquals(MemberListHandler.getInstance().getNumMembers(), new Integer(4));
+//		assertEquals(MemberListHandler.getInstance().getGlobal(), testUsernames);
+//		
+//	}
 	
 	@Test
 	public void testClearMethod(){
-		MemberListHandler handler = new MemberListHandler();
-		this.testHandler.clearMembers();
+		MemberListHandler.getInstance().clearMembers();
 
-		assertEquals(this.testHandler.getAssigned(), handler.getAssigned());
-		assertEquals(this.testHandler.getUnassigned(), handler.getUnassigned());
-		assertEquals(this.testHandler.getGlobal(), handler.getGlobal());
+		assertEquals(MemberListHandler.getInstance().getAssigned(), new ArrayList<String>());
+		assertEquals(MemberListHandler.getInstance().getUnassigned(), new ArrayList<String>());
+		assertEquals(MemberListHandler.getInstance().getGlobal(), new ArrayList<String>());
 	}
-	
-	@Test
-	public void testPopulate(){
-		
-		this.testHandler.addGlobal(testUsernames);
-		this.testHandler.populateMembers(testUsernamesAssigned);
-		
-		
-		assertEquals(this.testHandler.getAssigned(), this.testUsernamesAssigned);
-		assertEquals(this.testHandler.getUnassigned(), this.testUsernamesUnassigned);
-		assertEquals(this.testHandler.getNumAssigned(), new Integer(2));
-		assertEquals(this.testHandler.getNumUnAssigned(), new Integer(2));
-		assertEquals(this.testHandler.getNumMembers(), new Integer(4));
-		
-	}
-	
-	@Test
-	public void testAssignMember(){
-		this.testHandler.addGlobal(testUsernames);
-		
-		this.testHandler.assignMember(this.testUsernamesUnassigned.get(1));
-		
-		this.testUsernamesAssigned.add(this.testUsernamesUnassigned.get(1));
-		//this.testUsernamesUnassigned.remove(1);
-		System.out.println(testHandler.getAssigned());
-		System.out.println(testUsernamesAssigned);
-		
-		
-		assertEquals(this.testHandler.getAssigned(), this.testUsernamesAssigned);
-
-		assertEquals(this.testHandler.getUnassigned(), this.testUsernamesUnassigned);
-		assertEquals(this.testHandler.getNumAssigned(), new Integer(3));
-		assertEquals(this.testHandler.getNumUnAssigned(), new Integer(1));
-		assertEquals(this.testHandler.getGlobal(), new Integer(4));
-		
-		
-	}
-	
-	
+//	
+//	@Test
+//	public void testPopulate(){
+//		
+//		MemberListHandler.getInstance().addGlobal(testUsernames);
+//		MemberListHandler.getInstance().populateMembers(testUsernamesAssigned);
+//		
+//		
+//		assertEquals(MemberListHandler.getInstance().getAssigned(), this.testUsernamesAssigned);
+//		assertEquals(MemberListHandler.getInstance().getUnassigned(), this.testUsernamesUnassigned);
+//		assertEquals(MemberListHandler.getInstance().getNumAssigned(), new Integer(2));
+//		assertEquals(MemberListHandler.getInstance().getNumUnAssigned(), new Integer(2));
+//		assertEquals(MemberListHandler.getInstance().getNumMembers(), new Integer(4));
+//		
+//	}
+//	
+//	@Test
+//	public void testAssignMember(){
+//		MemberListHandler.getInstance().addGlobal(testUsernames);
+//		
+//		MemberListHandler.getInstance().assignMember(this.testUsernamesUnassigned.get(1));
+//		
+//		this.testUsernamesAssigned.add(this.testUsernamesUnassigned.get(1));
+//
+//		
+//		
+//		assertEquals(MemberListHandler.getInstance().getAssigned(), this.testUsernamesAssigned);
+//
+//		assertEquals(MemberListHandler.getInstance().getUnassigned(), this.testUsernamesUnassigned);
+//		assertEquals(MemberListHandler.getInstance().getNumAssigned(), new Integer(3));
+//		assertEquals(MemberListHandler.getInstance().getNumUnAssigned(), new Integer(1));
+//		assertEquals(MemberListHandler.getInstance().getGlobal(), new Integer(4));
+//		
+//		
+//	}
+//	
+//	
 	
 	
 	
 	
 }
-*/
+
 
