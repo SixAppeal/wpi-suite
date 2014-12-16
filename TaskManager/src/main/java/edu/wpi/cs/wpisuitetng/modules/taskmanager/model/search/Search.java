bd8be2660dacdad28840bef459ef5b54c2550d6f
Santiago-Rojas <srojas@wpi.edu>
2014-12-15 23:02:13 -0500

    Worked on filtering for archived tasks (added a checkbox for it) Added a way of displaying archived tasks

--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/model/search/Search.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/model/search/Search.java
@@ -52,21 +52,21 @@ public class Search {
 	private boolean isInit;
 	private Directory index;
 	private Directory temp_index = null;
-	
+
 	/**
 	 * General constructor for Search class
 	 */
 	public Search() {
 		isInit = false;
 	}
-	
+
 	/**
 	 * Initialize function
 	 */
 	public void initialize() {
 		isInit = true;
 	}
-	
+
 	/**
 	 * Main search method
 	 * @param input The string that user wants to search for
@@ -78,7 +78,7 @@ public class Search {
 	public List<Integer> searchFor(String input) throws SearchException, IOException, ParseException {
 		if (!isInit)
 			throw new SearchException("Search is not initialized.");
-		
+
 		if (this.temp_index != null) {
 			if (this.index != null) {
 				this.index.close();
@@ -86,75 +86,75 @@ public class Search {
 			this.index = temp_index;
 			this.temp_index = null;
 		}
-		
+
 		IndexReader reader = DirectoryReader.open(index);
 		IndexSearcher searcher = new IndexSearcher(reader);
 		Analyzer analyzer = new StandardAnalyzer();
-		
+
 		Query queryTitle = new QueryParser("title", analyzer).parse(input);
 		Query queryDescription = new QueryParser("description", analyzer).parse(input);
 		Query queryMembers = new QueryParser("assignedTo", analyzer).parse(input);
-		
+
 		TopScoreDocCollector collectorTitle = TopScoreDocCollector.create(10, true);
 		searcher.search(queryTitle, collectorTitle);
 		ScoreDoc[] hits = collectorTitle.topDocs().scoreDocs;
-		
+
 		List<Integer> toReturn = new ArrayList<Integer>();
 		Map<Integer, Integer> rankings = new HashMap<Integer, Integer>();
-		
-		
+
+
 		for(int i=0;i<hits.length;++i) {
-		    int docId = hits[i].doc;
-		    Document d = searcher.doc(docId);
-		    rankings.put(Integer.parseInt(d.get("id")), 3*(hits.length-i));
+			int docId = hits[i].doc;
+			Document d = searcher.doc(docId);
+			rankings.put(Integer.parseInt(d.get("id")), 3*(hits.length-i));
 		}
-		
+
 		TopScoreDocCollector collectorDescription = TopScoreDocCollector.create(10, true);
 		searcher.search(queryDescription, collectorDescription);
 		hits = collectorDescription.topDocs().scoreDocs;
-		
+
 		for(int i=0;i<hits.length;++i) {
-		    int docId = hits[i].doc;
-		    Document d = searcher.doc(docId);
-		    Integer id = Integer.parseInt(d.get("id"));
-		    if (rankings.containsKey(id)) {
-		    	rankings.put(id, 2*(hits.length -i) + rankings.get(id));
-		    }
-		    else {
-		    	rankings.put(id, 2*i);
-		    }
+			int docId = hits[i].doc;
+			Document d = searcher.doc(docId);
+			Integer id = Integer.parseInt(d.get("id"));
+			if (rankings.containsKey(id)) {
+				rankings.put(id, 2*(hits.length -i) + rankings.get(id));
+			}
+			else {
+				rankings.put(id, 2*i);
+			}
 		}
-		
+
 		TopScoreDocCollector collectorMembers = TopScoreDocCollector.create(10, true);
 		searcher.search(queryMembers, collectorMembers);
 		hits = collectorMembers.topDocs().scoreDocs;
-		
+
 		for(int i=0;i<hits.length;++i) {
-		    int docId = hits[i].doc;
-		    Document d = searcher.doc(docId);
-		    Integer id = Integer.parseInt(d.get("id"));
-		    if (rankings.containsKey(id)) {
-		    	rankings.put(id, (hits.length - i) + rankings.get(id));
-		    }
-		    else {
-		    	rankings.put(id, 2*i);
-		    }
+			int docId = hits[i].doc;
+			Document d = searcher.doc(docId);
+			Integer id = Integer.parseInt(d.get("id"));
+			if (rankings.containsKey(id)) {
+				rankings.put(id, (hits.length - i) + rankings.get(id));
+			}
+			else {
+				rankings.put(id, 2*i);
+			}
 		}
-		
+
 		PriorityQueue<IdRanking> idList = new PriorityQueue<IdRanking>();
-		
+
 		for (Map.Entry<Integer, Integer> i: rankings.entrySet()) {
 			idList.add(new IdRanking(i.getKey(), i.getValue()));
 		}
-		
+
 		while (!idList.isEmpty()) {
 			toReturn.add(idList.poll().getId());
 		}
-		
+
 		return toReturn;
-	
+
 	}
-	
+
 	/**
 	 * Creates index for each task, which is necessary for searching
 	 * @param taskList The list of tasks through which we will make an index for each one
@@ -169,7 +169,7 @@ public class Search {
 		indexTasks(writer, taskList);
 		writer.close();
 	}
-	
+
 	/**
 	 * Have a temp new index so that the search engine can use this
 	 * only when an update occurs such as when a task is created or a task is updated
@@ -186,7 +186,7 @@ public class Search {
 		writer.close();
 		this.temp_index = some_index;
 	}
-	
+
 	/**
 	 * Simply return boolean isInit variable
 	 * @return Return true if class is initialized. Else false.
@@ -194,7 +194,7 @@ public class Search {
 	public boolean isInitialized() {
 		return this.isInit;
 	}
-	
+
 	/**
 	 * Indexes the given file using the given writer, or if a directory is given,
 	 * recurses over files and directories found under the given directory
@@ -204,66 +204,68 @@ public class Search {
 	 */
 	private void indexTasks(IndexWriter writer, List<Task> taskList) throws IOException {		
 		for (Task t: taskList) {
-			
+
 			// make a new empty document
 			Document doc = new Document();
-			
+
 			String members = "";
 			for (String s: t.getAssignedTo()) {
 				members += s + " ";
 			}
 			// Make the primary search field the title of the task t
 			doc.add(new TextField("title", t.getTitle(), Field.Store.NO));
-			
+
 			// Add contents of description from task t
 			doc.add(new TextField("description", t.getDescription(), Field.Store.NO));
-			
+
 			// Add contents of assigned to member list from task t
 			doc.add(new TextField("assignedTo", members, Field.Store.NO));
-			
+
 			// Used to store task ID
 			doc.add(new StringField("id", Integer.toString(t.getId()), Field.Store.YES));
-			
+
 			writer.addDocument(doc);
 		}
 	}
-}
 
-/**
- * Primary purpose of this class is to implement compareTo method
- * @author akshoop
- * @author nhhughes
- */
-class IdRanking implements Comparable<IdRanking> {
-	private int id;
-	private Integer ranking;
-	
-	/**
-	 * Generic constructor
-	 * @param id ID of ranking
-	 * @param ranking Ranking of the class
-	 */
-	IdRanking(int id, Integer ranking) {
-		this.id = id;
-		this.ranking = ranking;
-	}
+
 
 	/**
-	 * Method to return the necessary priority queue for search
-	 * @return priority queue
+	 * Primary purpose of this class is to implement compareTo method
+	 * @author akshoop
+	 * @author nhhughes
 	 */
-	@Override
-	public int compareTo(IdRanking o) {
-		// The return is negative because PriorityQueue is low to high,
-		// but we want high to low because we want highest results first.
-		return -(this.ranking.compareTo(o.ranking));
-	}
-	
-	/**
-	 * Getter for ID
-	 * @return id The ID of IdRanking
-	 */
-	public int getId() {
-		return id;
+	class IdRanking implements Comparable<IdRanking> {
+		private int id;
+		private Integer ranking;
+
+		/**
+		 * Generic constructor
+		 * @param id ID of ranking
+		 * @param ranking Ranking of the class
+		 */
+		IdRanking(int id, Integer ranking) {
+			this.id = id;
+			this.ranking = ranking;
+		}
+
+		/**
+		 * Method to return the necessary priority queue for search
+		 * @return priority queue
+		 */
+		@Override
+		public int compareTo(IdRanking o) {
+			// The return is negative because PriorityQueue is low to high,
+			// but we want high to low because we want highest results first.
+			return -(this.ranking.compareTo(o.ranking));
+		}
+
+		/**
+		 * Getter for ID
+		 * @return id The ID of IdRanking
+		 */
+		public int getId() {
+			return id;
+		}
 	}
 }
--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/sidebar/SearchBox.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/sidebar/SearchBox.java
@@ -12,15 +12,21 @@
 
 package edu.wpi.cs.wpisuitetng.modules.taskmanager.view.sidebar;
 
-import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.BorderFactory;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
@@ -39,6 +45,7 @@ import edu.wpi.cs.wpisuitetng.modules.taskmanager.view.components.FormField;
  * @author akshoop
  * @author nhhughes
  * @author wavanrensselaer
+ * @author srojas
  */
 public class SearchBox extends JPanel implements IView {
 
@@ -48,17 +55,21 @@ public class SearchBox extends JPanel implements IView {
 	private static final long serialVersionUID = 2745494576347107370L;
 
 	private Gateway gateway;
-	
+
 	private JPanel container;
 	private JScrollPane scrollPane;
-	
+	private List<Integer> resultsG;
 	Search toSearch;
 	JTextField searchBox;
+	JCheckBox searchCheckBox;
 	JPanel resultsBox;
 	Form form;
 	GridBagConstraints gbc;
 	List<Task> taskList;
-	
+	JLabel archiveLabel;
+	boolean archiveModeOn;
+
+
 	/**
 	 * General constructor
 	 * @throws IOException 
@@ -66,25 +77,50 @@ public class SearchBox extends JPanel implements IView {
 	public SearchBox() throws IOException {
 		this.container = new JPanel();
 		this.scrollPane = new JScrollPane(this.container);
-		
+		this.archiveModeOn = false;
+		this.archiveLabel = new JLabel("Archived Tasks");
+
 		//this.scrollPane.setOpaque(false);
 		this.scrollPane.setBorder(BorderFactory.createEmptyBorder());
 		this.scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		
+
 		toSearch = new Search();
 		toSearch.initialize();
 		resultsBox = new JPanel();
-		
+
 		resultsBox.setLayout(new GridBagLayout());
 		resultsBox.setOpaque(false);
-		
+
+		this.searchCheckBox = new JCheckBox();
+
+
+		this.searchCheckBox.addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent arg0) {
+				if (archiveModeOn){
+					archiveModeOn = false;
+				}
+				else{
+					archiveModeOn = true;
+				}
+				displayResults(resultsG);
+			}
+		});
+
+
 		searchBox = new JTextField();
 		searchBox.setBorder(FormField.BORDER_NORMAL);
 		searchBox.addKeyListener(new SearchUserInput(this.searchBox, this.toSearch, this));
+		searchBox.addActionListener( new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				resultsG.clear();
+				displayResults(resultsG);
+			}});
 
 		this.container.setLayout(new GridBagLayout());
 		this.container.setBackground(SidebarView.SIDEBAR_COLOR);
-		
+
 		gbc = new GridBagConstraints();
 		gbc.anchor = GridBagConstraints.PAGE_START;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
@@ -92,13 +128,28 @@ public class SearchBox extends JPanel implements IView {
 		gbc.weightx = 1.0;
 		gbc.gridx = 0;
 		gbc.gridy = 0;
+		gbc.gridwidth = 2;
 		this.container.add(searchBox, gbc);
-		
+
+		gbc.insets = new Insets(10, 20, 10, 10);
+		gbc.weightx = 0;
+		gbc.weighty = 0;
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.gridwidth = 1;
+		this.container.add(searchCheckBox, gbc);
+
+		gbc.weightx = 1;
+		gbc.weighty = 0;
+		gbc.gridx = 1;
+		gbc.gridy = 1;
+		this.container.add(archiveLabel, gbc);
+
 		gbc.insets.top = 0;
 		gbc.weighty = 1.0;
-		gbc.gridy = 1;
+		gbc.gridy = 2;
 		this.container.add(resultsBox, gbc);
-		
+
 		this.scrollPane.setMinimumSize(new Dimension(300, 0));
 		this.setLayout(new MigLayout("fill, ins 0", "[300]"));
 		this.add(this.scrollPane, "grow");
@@ -110,35 +161,55 @@ public class SearchBox extends JPanel implements IView {
 	 */
 	public void displayResults(List<Integer> results) {
 		this.resultsBox.removeAll();
-		
+		resultsG =results;
 		gbc.gridx = 0;
 		gbc.gridy = 0;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
-		
+
 		int count = 0;
-		
-		for (Integer r: results) {
-			gbc.fill = GridBagConstraints.HORIZONTAL;
-			Task t = getTask(r);
-			
-			TaskView content = new TaskView(t, true);
-			content.setGateway(this.gateway);
-			gbc.gridy = count;
-			this.resultsBox.add(content, gbc);
-			count++;
+		if (archiveModeOn && (searchBox.getText().isEmpty())){
+			for (Task t : taskList){
+				TaskView content = new TaskView(t, true);
+				content.setGateway(this.gateway);
+				gbc.gridy = count;
+				this.resultsBox.add(content, gbc);
+				count++;
+			}
 		}
-		
+		else {
+			for (Integer r: results) {
+				gbc.fill = GridBagConstraints.HORIZONTAL;
+				Task t = getTask(r);
+
+				if (archiveModeOn && t.isArchived()){
+					TaskView content = new TaskView(t, true);
+					content.setGateway(this.gateway);
+					gbc.gridy = count;
+					this.resultsBox.add(content, gbc);
+					count++;
+				}
+				else if ((!archiveModeOn && (!t.isArchived()))) {
+					TaskView content = new TaskView(t, true);
+					content.setGateway(this.gateway);
+					gbc.gridy = count;
+					this.resultsBox.add(content, gbc);
+					count++;
+				}
+
+			}
+		}
+
 		gbc.weighty = 1.0;
 		gbc.gridy = count + 1;
 		JPanel filler = new JPanel();
 		filler.setOpaque(false);
-		
+
 		this.resultsBox.add(filler, gbc);
 		gbc.weighty = 0.0;
 		this.revalidate();
 		this.repaint();
 	}
-	
+
 	/**
 	 * This gets a task from the given ID
 	 * @param id The specific task ID
@@ -152,7 +223,7 @@ public class SearchBox extends JPanel implements IView {
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Views a task from inputed task
 	 * @param t Task to view
@@ -160,7 +231,7 @@ public class SearchBox extends JPanel implements IView {
 	public void viewTask(Task t) {
 		this.gateway.toPresenter("TaskPresenter", "editTask", t);
 	}
-	
+
 	/**
 	 * Retrieves tasks from the local cache
 	 * @return tasks_from_cache An array of tasks retrieved
@@ -168,7 +239,7 @@ public class SearchBox extends JPanel implements IView {
 	 */
 	public void updateIndex(ArrayList<Task> all_tasks) throws IOException {
 		this.taskList = all_tasks;
-		
+
 		if (this.toSearch.isInitialized()) {
 			this.toSearch.updateIndex(all_tasks);
 		}
@@ -177,7 +248,7 @@ public class SearchBox extends JPanel implements IView {
 			this.toSearch.createIndex(all_tasks);
 		}
 	}
-	
+
 	/**
 	 * @see IView.setGateway
 	 * @param gateway
