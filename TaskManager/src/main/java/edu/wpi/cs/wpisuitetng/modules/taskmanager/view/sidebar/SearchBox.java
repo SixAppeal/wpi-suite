5857def127f4446ab8d33b8130167263147add27
William Temple <wmtemple@wpi.edu>
2014-12-17 03:44:13 -0500

    Cleaned up Archive Search Checkbox, added names to color categories.

--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/sidebar/SearchBox.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/sidebar/SearchBox.java
@@ -166,14 +166,14 @@ public class SearchBox extends JPanel implements IView {
 		gbc = new GridBagConstraints();
 		gbc.anchor = GridBagConstraints.PAGE_START;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
-		gbc.insets = new Insets(20, 20, 20, 20);
+		gbc.insets = new Insets(20, 20, 0, 20);
 		gbc.weightx = 1.0;
 		gbc.gridx = 0;
 		gbc.gridy = 0;
 		gbc.gridwidth = 2;
 		this.container.add(searchBox, gbc);
 
-		gbc.insets = new Insets(10, 20, 10, 10);
+		gbc.insets = new Insets(5, 20, 0, 0);
 		gbc.weightx = 0;
 		gbc.weighty = 0;
 		gbc.gridx = 0;
@@ -181,13 +181,14 @@ public class SearchBox extends JPanel implements IView {
 		gbc.gridwidth = 1;
 		this.container.add(searchCheckBox, gbc);
 
+		gbc.insets = new Insets(8, 5, 0, 20);
 		gbc.weightx = 1;
 		gbc.weighty = 0;
 		gbc.gridx = 1;
 		gbc.gridy = 1;
 		this.container.add(archiveLabel, gbc);
 
-		gbc.insets = new Insets(10, 10, 10, 10);
+		gbc.insets = new Insets(5, 10, 10, 10);
 		//gbc.insets.top = 0;
 		gbc.gridwidth = 2; 
 		gbc.gridx = 0;
