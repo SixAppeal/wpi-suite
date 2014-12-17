ebd398853385607e0b64d7d1d92c1875e7fd3162
rorlando <ejunk0213@comcast.net>
2014-12-16 23:10:22 -0500

    Fixed the sidebar
    Switching error no longer happens

--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/sidebar/SidebarView.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/sidebar/SidebarView.java
@@ -122,6 +122,22 @@ public class SidebarView extends JTabbedPane implements IView {
 		
 	}
 	
+	public void selectComponent(IView view)
+	{
+		for(int i = 0; i < this.getTabCount(); i++)
+		{
+			Component k = this.getComponentAt(i);
+			if(k instanceof EmptyComponentHolder)
+			{
+				if(((EmptyComponentHolder) k).contents.equals(view))
+				{
+					this.setSelectedComponent(k);
+				}
+			}
+		}
+		
+	}
+	
 	/**
 	 * Uncompates the current file
 	 * and Compates all other files
@@ -166,7 +182,7 @@ public class SidebarView extends JTabbedPane implements IView {
 		for (IView view : viewList) {
 			if (view instanceof TaskCreateView) {
 				if (((TaskCreateView) view).isEmpty()) {
-					this.setSelectedComponent((TaskCreateView)view);
+					this.selectComponent((TaskCreateView)view);
 					return;
 				}
 			}
@@ -205,7 +221,7 @@ public class SidebarView extends JTabbedPane implements IView {
 		for (IView view : viewList) {
 			if (view instanceof TaskEditView) {
 				if (task.equals(((TaskEditView) view).getTask())) {
-					setSelectedComponent((TaskEditView)view);
+					this.selectComponent((TaskEditView)view);
 					((TaskEditView) view).getRequirements((Requirement[])cache.retrieve("requirements"));
 					return;
 				}
--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/sidebar/TaskEditView.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/sidebar/TaskEditView.java
@@ -77,6 +77,7 @@ import edu.wpi.cs.wpisuitetng.modules.taskmanager.view.components.MemberButtonGr
  * @author akshoop
  * @author dpseaman
  * @author thhughes
+ * @author rnorlando
  */
 public class TaskEditView extends JPanel implements IView {
 	private static final long serialVersionUID = -8972626054612267276L;
@@ -664,5 +665,25 @@ public class TaskEditView extends JPanel implements IView {
 		FontMetrics fm = this.requirementsComboBox.getFontMetrics((this.requirementsComboBox.getFont()));
 		String shortenedTitle = TaskManagerUtil.reduceString(aReq.getName(), 210, fm);
 		return shortenedTitle;
+	}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		if(!obj.getClass().equals(this.getClass()))
+		{
+			return false;
+		}
+		
+		if (obj instanceof TaskEditView) 
+		{
+			TaskEditView that = (TaskEditView) obj;
+			if (this.task.equals(that.getTask())) 
+			{
+				return true;
+			}
+		}
+		
+		return false;
 	}
 }
